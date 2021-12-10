0e2d7439fc76e3c03933437ecb2a0fc9a7f84601
Hamza Aamir <hamzaamirm2001@gmail.com>
2021-12-10 18:37:55 +0500

    Project Finished

--- js/script.js
+++ js/script.js
@@ -1,3 +1,31 @@
 const slides = document.querySelectorAll('.slide');
 const prevBtn = document.querySelector('.prev');
 const nextBtn = document.querySelector('.next');
+
+slides.forEach(function (slide, index){
+    slide.style.left = `${index * 100}%`;
+});
+
+let counter = 0;
+
+nextBtn.addEventListener('click', function(){
+    counter++;
+    carousel();
+});
+
+prevBtn.addEventListener('click', function(){
+    counter--;
+    carousel();
+});
+
+function carousel(){
+    if(counter == slides.length){
+        counter = 0;
+    }
+    else if(counter < 0){
+        counter = slides.length - 1;
+    }
+    slides.forEach(function(slide){
+        slide.style.transform = `translateX(-${counter * 100}%)`;
+    });
+}
